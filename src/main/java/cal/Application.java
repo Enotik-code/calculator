b8060906661d23eb98f80931907cedc502c99e26
kupreichikpavel <kupreichik.p@gmail.com>
2021-09-26 19:28:15 +0300

    внес строку с важной инфой

--- src/main/java/cal/Application.java
+++ src/main/java/cal/Application.java
@@ -1,8 +1,12 @@
 package cal;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println("Введите первое число:");
-        //Задача Павла
+        String string = s.nextLine();
+        System.out.println("что ты написал " + string);
     }
 }
