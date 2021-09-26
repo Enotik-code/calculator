f02866d615d27c82be1a82b5db53f26a2c4b8ae1
kupreichikpavel <kupreichik.p@gmail.com>
2021-09-26 16:11:58 +0300

    внес строку с важной инфой

--- src/main/java/cal/Application.java
+++ src/main/java/cal/Application.java
@@ -3,5 +3,6 @@ package cal;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Готовим коды");
+        System.out.println("Сам пиши свой калькулятор, что ты докаплся до меня");
     }
 }
