ea7667a3130840a79abac04c79e7112ff19b7fe3
Enotik-code <fvantop@gmail.com>
2021-09-26 16:20:38 +0300

    обновил задачи

--- src/main/java/cal/Application.java
+++ src/main/java/cal/Application.java
@@ -2,7 +2,7 @@ package cal;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Готовим коды");
-        System.out.println("Сам пиши свой калькулятор, что ты докаплся до меня");
+        System.out.println("Введите первое число:");
+        //Задача Павла
     }
 }
